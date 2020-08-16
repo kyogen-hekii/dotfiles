78159071cfffbeefcb9faa94bbc495deb16eaa16
kyogen <kyogen@maedakentanoMacBook.local>
2020-08-16 10:33:48 +0900

    mac用のvscodeの設定,karabiner編集

---
+++ config/vscode/mac/link_as_deploy_vscode_settings.sh
@@ -0,0 +1,11 @@
+THIS_DIRECTORY=$(cd $(dirname $0)/; pwd)
+VSCODE_DIRECTORY="$HOME/Library/Application\ Support/Code/User"
+
+# シンボリックリンク作成
+ln -snfv "$THIS_DIRECTORY/keybindings.json" "$VSCODE_DIRECTORY/keybindings.json"
+ln -snfv "$THIS_DIRECTORY/settings.json" "$VSCODE_DIRECTORY/settings.json"
+ln -snfv "$THIS_DIRECTORY/snippets" "$VSCODE_DIRECTORY/snippets"
+
+# typescriptとtypescriptreactを合わせる
+ln -snfv $VSCODE_DIRECTORY/snippets/typescriptreact.json \
+  $VSCODE_DIRECTORY/snippets/typescript.json
