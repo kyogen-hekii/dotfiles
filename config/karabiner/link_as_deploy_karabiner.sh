d906ab2c84a88c33445a10044e66a09345844d36
k <kokoro.ok.9n@gmail.com>
2024-08-10 23:45:08 +0900

    fix: mac用にkeybinding設定修正

--- config/karabiner/link_as_deploy_karabiner.sh
+++ config/karabiner/link_as_deploy_karabiner.sh
@@ -1,6 +1,6 @@
 THIS_DIRECTORY=$(cd $(dirname $0)/; pwd)
 # bkupとoriginを作成
 cp -f "$HOME/.config/karabiner/karabiner.json" "$HOME/.config/karabiner/karabiner_origin.json"
-cp -f "$THIS_DIRECTORY/karabiner.json" "$HOME/.config/karabiner/karabiner_bkup.json"
+cp -f "$THIS_DIRECTORY/karabiner.json" "$HOME/.config/karabiner/karabiner_dotfiles.json"
 # シンボリックリンク作成
 ln -snfv "$THIS_DIRECTORY/karabiner.json" "$HOME/.config/karabiner/karabiner.json"
