f17a5e1682c53defcda41176907c42c5687cf6b5
k <kokoro.ok.9n@gmail.com>
2024-08-11 17:00:08 +0900

    fix: dev-container用の設定修正

--- scripts/preinstall_devcontainer.sh
+++ scripts/preinstall_devcontainer.sh
@@ -1,19 +1,27 @@
 
 # TODO: 個別ファイルにせずまとめる
 
-main() {
-  if ! is_exists zsh; then
-    # zsh
-    sudo apt install zsh
-    chsh -s $(which zsh)
-    # 戻すとき: sudo /bin/chsh /bin/sh
-  fi
-}
+DOTFILES_PATH=~/dotfiles
 
-# utils
-is_exists() {
-  which "$1" >/dev/null 2>&1
-  return $?
+main() {
+  sudo dnf install -y util-linux-user which make ripgrep fd-find gcc cmake pkg-config libssl-dev
+
+  sudo dnf install -y zsh
+  chsh -s $(which zsh)
+  # 戻すとき: sudo /bin/chsh /bin/sh
+
+  curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+  export PATH=$HOME/.cargo/bin:$PATH
+  cargo install eza exa bat tre-command
+
+  cargo install starship --locked
+  [ ! -d ~/.config ] && mkdir ~/.config
+  cp $DOTFILES_PATH/config/starship_initial.toml ~/.config/starship.toml
+
+  cargo install sheldon --locked
+  [ ! -d ~/.config/sheldon ] && mkdir ~/.config/sheldon
+  cp $DOTFILES_PATH/config/sheldon_plugins_initial.toml ~/.config/sheldon/plugins.toml
+
 }
 
 main
