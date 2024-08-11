82e26ba7a940346a18dfdb2871a483e054825593
k <kokoro.ok.9n@gmail.com>
2024-08-11 18:11:00 +0900

    fix: dev-container用の設定修正

--- scripts/preinstall_devcontainer.sh
+++ scripts/preinstall_devcontainer.sh
@@ -4,10 +4,10 @@
 DOTFILES_PATH=~/dotfiles
 
 main() {
-  sudo dnf install -y util-linux-user which make ripgrep fd-find gcc cmake pkg-config libssl-dev
+  sudo dnf install -y util-linux-user which make gcc cmake openssl-devel
 
   sudo dnf install -y zsh
-  chsh -s $(which zsh)
+  sudo chsh -s $(which zsh)
   # 戻すとき: sudo /bin/chsh /bin/sh
 
   curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
@@ -22,6 +22,22 @@ main() {
   [ ! -d ~/.config/sheldon ] && mkdir ~/.config/sheldon
   cp $DOTFILES_PATH/config/sheldon_plugins_initial.toml ~/.config/sheldon/plugins.toml
 
+  eval "$(starship init zsh)"
+
+  autoload -Uz compinit
+  compdef
+  eval "$(sheldon source)"
 }
 
+prepare_font() {
+  mkdir ~/tmp
+  curl -L https://github.com/yuru7/HackGen/releases/download/v2.9.0/HackGen_NF_v2.9.0.zip --output ~/tmp/HackGen_NF.zip
+  # unzipはworkdirに作られる
+  unzip ~/tmp/HackGen_NF.zip
+  rm ~/tmp/HackGen_NF.zip
+  sudo mkdir /usr/local/share/fonts
+  sudo mv HackGen_NF_v2.9.0 /usr/local/share/fonts/HackGen_NF
+  fc-cache -fv
+}
 main
+prepare_font
